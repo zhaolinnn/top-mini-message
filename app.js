d0eaeeabf05bad06867745984bfc2f07b18769f0
alanliu <65645504+AlanL318@users.noreply.github.com>
2025-12-27 23:41:36 -0500

    Added fake message db and added to index page

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const indexRouter = require("./routes/indexRouter");
 const newRouter = require("./routes/newRouter");
+// set for ejs
+const path = require("node:path");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use("/new", newRouter);
 app.use("/", indexRouter);
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,11 +1,22 @@
 const { Router } = require("express");
 
 const messages = [
-    
-]
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date()
+    }
+];
 
 const indexRouter = Router();
 
-indexRouter.get("/", (req, res) => res.send("testerss!!"));
+indexRouter.get("/", (req, res) => {
+    res.render("index", { messages: messages });
+});
 
 module.exports = indexRouter;
