be773382ed17662c0c8469a4e99c652a5896d0d7
alanliu <65645504+AlanL318@users.noreply.github.com>
2025-12-29 14:22:32 -0500

    fix json file again

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/new", newRouter);
 app.use("/", indexRouter);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, (error) => {
   if (error) {
